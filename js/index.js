5f618b412bc85ac3f441c6a0ee71ee78f44b2dbe
nico9812 <nicocalderon9818@gmail.com>
2025-11-26 15:14:34 -0300

    init

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// Fecha del evento
+const eventDate = new Date("2025-12-12T22:20:30").getTime();
+
+function actualizarCountdown() {
+    const now = new Date().getTime();
+    const diff = eventDate - now;
+
+    if (diff <= 0) return;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    document.getElementById("cd-days").innerText = days;
+    document.getElementById("cd-hours").innerText = hours;
+    document.getElementById("cd-minutes").innerText = minutes;
+    document.getElementById("cd-seconds").innerText = seconds;
+}
+
+setInterval(actualizarCountdown, 1000);
+actualizarCountdown();
+
+// ----------------------
+// MODAL
+// ----------------------
+
+const modal = document.getElementById("modalConfirmar");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.querySelector(".close");
+const span = document.getElementById("spanerror");
+
+openBtn.onclick = () => {
+    modal.style.display = "flex";
+    span.style.display="none";
+};
+closeBtn.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+    if (e.target === modal)modal.style.display = "none";
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        const formData = new URLSearchParams();
+        formData.append("entry.1868369155",document.getElementById("nombre").value);
+        fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLScAAMXVYI_0UZ4dSso_FW9F3gmWJwnfD_NCUUCdkg0ZJqgCdw/formResponse",{
+            method: "POST",
+            body:formData,
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded"
+            },
+            mode:"no-cors"
+        }).then(()=>{
+            form.reset()
+            modal.style.display = "none"
+            setTimeout(cerrarmodalok, 2000);
+
+        }).catch(error =>{
+            span.style.display="block";
+        });
+    });
+
+});
+
+
